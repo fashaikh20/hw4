b5566134c3aaa435e3980b7e6d8a70fdb825adfa
fashaikh20 <faisal.shaikh@kellogg.northwestern.edu>
2022-03-13 23:08:07 +0000

    still logged out not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,22 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.first_name}."
-        redirect_to "/companies"
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
       else
-        flash[:notice] = "Nope"
+        flash[:notice] = "Login unsuccessful, please check your username and password"
         redirect_to "/sessions/new"
       end
     else
       flash[:notice] = "Nope"
       redirect_to "/sessions/new"
     end
-    
+
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Goodbye, #{@user.username}."
     redirect_to "/sessions/new"
 
 
